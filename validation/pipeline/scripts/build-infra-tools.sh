5303ed458c612ac0f4ea159fea0192a2bf1181fb
Daniel Newman <dan@danlovesprogramming.com>
2025-12-23 14:11:42 -0600

    Fix build-infra-tools.sh script path for Dockerfile validation

--- validation/pipeline/scripts/build-infra-tools.sh
+++ validation/pipeline/scripts/build-infra-tools.sh
@@ -2,5 +2,7 @@
 set -e
 
 # Build the infrastructure tools Docker image
+# Script is executed from validation/pipeline/scripts directory via container.build()
+# Need to go up two levels to reach tests directory from workspace root
 docker build --platform linux/amd64 -t rancher-infra-tools:latest \
-  -f ./tests/validation/Dockerfile.validation .
+  -f ../../tests/validation/Dockerfile.validation .
